d7d92ea75d5a5fd07ee5c2bbf8ea37605baf746d
alimaher <ali.maher0013@gmail>
2024-10-31 16:15:35 +0300

    adding font asset & images assets and making styles files

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: prefer_single_quotes
+class AssetsImages {
+  AssetsImages._();
+  
+  static const String imagesAvatar1svg = "assets/images/avatar1svg.svg";
+
+  static const String imagesAvatar2svg = "assets/images/avatar2svg.svg";
+
+  static const String imagesCardBackground = "assets/images/card_background.svg";
+
+  static const String imagesCustomerNamesvg = "assets/images/customerNamesvg.svg";
+
+  static const String imagesDashboard = "assets/images/dashboard.svg";
+
+  static const String imagesExpensessvg = "assets/images/expensessvg.svg";
+
+  static const String imagesIncome = "assets/images/income.svg";
+
+  static const String imagesInvestment = "assets/images/investment.svg";
+
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  static const String imagesMoneys = "assets/images/moneys.svg";
+
+  static const String imagesMyTransaction = "assets/images/My_Transaction.svg";
+
+  static const String imagesSetting2 = "assets/images/setting-2.svg";
+
+  static const String imagesStatisticssvg = "assets/images/statisticssvg.svg";
+
+  static const String imagesWallet2 = "assets/images/wallet-2.svg";
+}
+
--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -16,6 +16,5 @@ class DashboardView extends StatelessWidget {
         },
       ),
     );
-    ;
   }
 }
